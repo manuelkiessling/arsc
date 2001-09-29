acef55f0d71cf4780cfc27bd133f6f0cbeb39bbe
Manuel Kießling <manuel@kiessling.net>
2001-09-29 01:57:15 +0000

    A very special version that will hopefully make ARSC available for the ZUUM.de WebTV Box
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@37 e1bb756e-f405-4550-b03d-8a9c705c3ac8

---
+++ trunk/latest/version_box/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+ <head>
+  <title>
+   ARSC - ARSC Really Simple Chat
+  </title>
+ </head>
+ <frameset rows="*,40" border="1" framespacing="no" frameborder="0" marginwidth="2" marginheight="1">
+  <frame src="chatmsg.php?arsc_sid=<?php echo $arsc_sid; ?>#end" NAME="msg" scrolling="auto" noresize marginwidth="2" marginheight="1">
+  <frame src="chatinput.php?arsc_sid=<?php echo $arsc_sid; ?>" name="input"  scrolling="no" noresize marginwidth="2" marginheight="1">
+ </frameset>
+ <noframes>
+  Sorry, ARSC needs a browser that understands framesets.
+ </noframes>
+</html>
