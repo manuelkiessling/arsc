7bb9f19b0d6772ca059aa16d4f4769edd1137e1a
Manuel Kießling <manuel@kiessling.net>
2001-11-30 18:59:05 +0000

    most actual table layout
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@60 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/latest/install.php
+++ trunk/latest/install.php
@@ -1,32 +1,62 @@
 <?php
 
 include("config.inc.php");
+include("functions.inc.php");
 
-if ($result = mysql_query("CREATE TABLE arsc_room_lounge (
-                            id int(11) NOT NULL auto_increment,
-                            message text NOT NULL,
-                            user varchar(64) NOT NULL,
-                            sendtime time DEFAULT '00:00:00' NOT NULL,
-                            timeid bigint(20) NOT NULL,
-                            PRIMARY KEY (id),
-                            KEY timeid (timeid)
-                           )")
-              &&
-              mysql_query("CREATE TABLE arsc_users (
-                            id int(11) NOT NULL auto_increment,
-                            user varchar(10) NOT NULL,
-                            lastping int(11) NOT NULL,
-                            ip varchar(15) NOT NULL,
-                            room varchar(32) NOT NULL,
-                            language varchar(32) NOT NULL,
-                            version varchar(16) NOT NULL,
-                            level int(11) NOT NULL,
-                            sid varchar(32) NOT NULL,
-                            lastmessageping bigint(20) NOT NULL,
-                            PRIMARY KEY (id),
-                            KEY lastping (lastping),
-                            KEY user (user)
-                           )"))
+if (mysql_query("CREATE TABLE arsc_bannlist
+                 (
+                  id int(11) NOT NULL auto_increment,
+                  ip varchar(15) NOT NULL default '',
+                  until int(11) NOT NULL default '0',
+                  PRIMARY KEY  (id),
+                  KEY ip (ip)
+                 )
+                 TYPE=MyISAM")
+    &&
+    mysql_query("CREATE TABLE arsc_room_lounge
+                 (
+                  id int(11) NOT NULL auto_increment,
+                  message text NOT NULL,
+                  user varchar(64) NOT NULL default '',
+                  flag_ripped tinyint(4) NOT NULL default '0',
+                  sendtime time NOT NULL default '00:00:00',
+                  timeid bigint(20) NOT NULL default '0',
+                  PRIMARY KEY  (id),
+                  KEY timeid (timeid)
+                 )
+                 TYPE=MyISAM")
+    &&
+    mysql_query("CREATE TABLE arsc_registered_users
+                 (
+                  id int(11) NOT NULL auto_increment,
+                  user varchar(10) NOT NULL default '',
+                  password varchar(64) NOT NULL default '',
+                  email text NOT NULL,
+                  flag_locked tinyint(4) NOT NULL default '0',
+                  PRIMARY KEY  (id),
+                  KEY user (user)
+                 )
+                 TYPE=MyISAM")
+    &&
+    mysql_query("CREATE TABLE arsc_users
+                 (
+                  id int(11) NOT NULL auto_increment,
+                  user varchar(10) NOT NULL default '',
+                  lastping int(11) NOT NULL default '0',
+                  ip varchar(15) NOT NULL default '',
+                  room varchar(32) NOT NULL default '',
+                  language varchar(32) NOT NULL default '',
+                  version varchar(16) NOT NULL default '',
+                  level int(11) NOT NULL default '0',
+                  flag_ripped tinyint(4) NOT NULL default '0',
+                  sid varchar(32) NOT NULL default '',
+                  lastmessageping bigint(20) NOT NULL default '0',
+                  PRIMARY KEY  (id),
+                  KEY lastping (lastping),
+                  KEY user (user)
+                 )
+                 TYPE=MyISAM")
+   )
 {
  echo "ARSC is installed. Please delete this file (install.php) from your webserver now!";
 }
