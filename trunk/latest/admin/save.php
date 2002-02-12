23dccaa0a02b87b7d04e29279e2b27ed9e86a392
Manuel Kießling <manuel@kiessling.net>
2002-02-12 21:52:46 +0000

    fields that not yet exist will be inserted now instead of trying to update them.
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@124 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/latest/admin/save.php
+++ trunk/latest/admin/save.php
@@ -8,13 +8,27 @@ set_magic_quotes_runtime(1);
 reset($arsc_save_parameters);
 while (list($key, $val) = each($arsc_save_parameters))
 {
- mysql_query("UPDATE arsc_parameters SET value = '".stripslashes($val)."' WHERE name = '$key'");
+ if (mysql_query("SELECT value FROM arsc_parameters WHERE name = '$key'"))
+ {
+  mysql_query("UPDATE arsc_parameters SET value = '".stripslashes($val)."' WHERE name = '$key'");
+ }
+ else
+ {
+  mysql_query("INSERT INTO arsc_parameters (name, value) VALUES ('$key', '".stripslashes($val)."')");
+ }
 }
 
 reset($arsc_save_parameters_smilies);
 while (list($key, $val) = each($arsc_save_parameters_smilies))
 {
- mysql_query("UPDATE arsc_parameters_smilies SET value = '".stripslashes($val)."' WHERE id = '$key'");
+ if (mysql_query("SELECT value FROM arsc_parameters WHERE id = '$key'"))
+ {
+  mysql_query("UPDATE arsc_parameters_smilies SET value = '".stripslashes($val)."' WHERE id = '$key'")
+ }
+ else
+ {
+  mysql_query("INSERT INTO arsc_parameters (id, value) VALUES ('$key', '".stripslashes($val)."')");
+ }
 }
 
 header("Location: index.php?arsc_message=".urlencode("Parameters successfully saved."));
