64c0b4876aa9c04c4b8c2c86214cae83a9ae9e07
Manuel Kießling <manuel@kiessling.net>
2002-10-24 15:56:48 +0000

    finally a nice index page
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@163 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/latest/index.php
+++ trunk/latest/index.php
@@ -1,4 +1,5 @@
 <?php include("config.inc.php"); ?>
+<?php include("functions.inc.php"); ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html>
  <head>
@@ -7,27 +8,37 @@
   </title>
  </head>
  <body bgcolor="#FFFFFF">
+  <br>
+  <br>
   <form action="home.php" METHOD="POST">
-   <?php
-   $handle = opendir("shared/language");
-   while ($file = readdir($handle))
-   { 
-    if ($file != "." && $file != "..")
-    { 
-     $arsc_language = str_replace(".inc.php", "", $file);
-     ?>
-     <input type="radio" name="arsc_language" value="<?php echo $arsc_language; ?>">
-     <font face="Arial" size="2">
-      <?php echo ucfirst($arsc_language); ?>
-     </font>
-     <br>
-     <?php
-    } 
-   }
-   closedir($handle);
-   ?>
-   <br>
-   <input type="submit" value="OK">
+   <table align="center" cellpadding="6" bgcolor="<?php echo $arsc_parameters["color_userlist_window_background"]; ?>">
+    <tr>
+     <td>
+      <font face="Arial, Verdana, sans-serif">
+       <b>
+        Choose your language:
+       </b>
+      </font>
+      <select name="arsc_language">
+       <?php
+       $handle = opendir("shared/language");
+       while ($file = readdir($handle))
+       { 
+        if ($file != "." && $file != "..")
+        { 
+         $arsc_language = str_replace(".inc.php", "", $file);
+         ?>
+         <option value="<?php echo $arsc_language; ?>"<?php if ($arsc_language == $arsc_parameters["standard_language"]) echo " selected"; ?>><?php echo ucfirst($arsc_language); ?></option>
+         <?php
+        } 
+       }
+       closedir($handle);
+       ?>
+      </select>
+      <input type="submit" value="Go &gt;">
+     </td>
+    </tr>
+   </table>
   </form>
  </body>
 </html>
