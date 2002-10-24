6aab34fe2e2a7499628cac7e132a333d82157a74
Manuel Kießling <manuel@kiessling.net>
2002-10-24 13:48:39 +0000

    Change password function now working
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@156 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/latest/changepassword.php
+++ trunk/latest/changepassword.php
@@ -23,7 +23,14 @@ if ($arsc_send == "")
         <?php
         if ($arsc_error <> "")
         {
-         echo $arsc_lang["error_".$arsc_error];
+         if ($arsc_lang["error_".$arsc_error] <> "")
+         {
+          echo $arsc_lang["error_".$arsc_error];
+         }
+         else
+         {
+          echo $arsc_error;
+         }
         }
         ?>
        </b>
@@ -32,13 +39,7 @@ if ($arsc_send == "")
        <input type="hidden" name="arsc_send" value="yes">
        <input type="hidden" name="arsc_language" value="<?php echo $arsc_language; ?>">
        <font face="Arial" size="2">
-        <?php
-        echo $arsc_lang["register_intro"]."\n";
-        if ($arsc_parameters["register_force"] == "yes")
-        {
-         echo $arsc_lang["register_intro_force"];
-        }
-        ?>
+        <?php echo $arsc_lang["changepassword_intro"]; ?>
        </font>
        <br>
        <br>
@@ -47,47 +48,40 @@ if ($arsc_send == "")
          <td>
           <font face="Arial" size="2">
            <b>
-            <?php echo $arsc_lang["register_entername"]; ?>
+            <?php echo $arsc_lang["changepassword_entername"]; ?>
            </b>
           </font>
          </td>
          <td>
-          <input type="text" name="arsc_user" size="12" maxlength="12">
+          <input type="text" name="arsc_user" size="12" maxlength="12" value="<?php echo $arsc_user; ?>">
          </td>
         </tr>
-        <?php
-        if ($arsc_parameters["register_force"] <> "yes")
-        {
-         ?>
-         <tr>
-          <td>
-           <font face="Arial" size="2">
-            <b>
-             <?php echo $arsc_lang["register_enterpassword"]; ?>
-            </b>
-           </font>
-          </td>
-          <td>
-           <input type="password" name="arsc_password" size="12" maxlength="12">
-          </td>
-         </tr>
-         <?php
-        }
-        ?>
         <tr>
          <td>
           <font face="Arial" size="2">
            <b>
-            <?php echo $arsc_lang["register_enteremail"]; ?>
+            <?php echo $arsc_lang["changepassword_entercurrent"]; ?>
            </b>
           </font>
          </td>
          <td>
-          <input type="text" name="arsc_email" size="12" value="<?php echo $arsc_email; ?>">
+          <input type="password" name="arsc_password_old" size="12" maxlength="12">
+         </td>
+        </tr>
+        <tr>
+         <td>
+          <font face="Arial" size="2">
+           <b>
+            <?php echo $arsc_lang["changepassword_enternew"]; ?>
+           </b>
+          </font>
+         </td>
+         <td>
+          <input type="password" name="arsc_password_new" size="12" maxlength="12">
          </td>
         </tr>
        </table>
-       <input type="submit" value="<?php echo $arsc_lang["register_send"]; ?>">
+       <input type="submit" value="<?php echo $arsc_lang["changepassword_submit"]; ?>">
       </form>
      </td>
     </tr>
@@ -98,24 +92,17 @@ if ($arsc_send == "")
 }
 else
 {
- $arsc_result = mysql_query("SELECT COUNT(*) as howmany FROM arsc_registered_users WHERE user = '$arsc_user'");
+ $arsc_result = mysql_query("SELECT COUNT(id) AS howmany FROM arsc_registered_users WHERE user = '$arsc_user' AND password = '$arsc_password_old'");
  $arsc_a = mysql_fetch_array($arsc_result);
  if ($arsc_a["howmany"] == 0)
  {
-  $arsc_location = "Location: home.php?arsc_language=".$arsc_language."&arsc_user=".$arsc_user;
-  if ($arsc_parameters["register_force"] == "yes")
-  {
-   $arsc_password = substr(md5(time()), 0,5);
-   $arsc_location = "Location: home.php?arsc_language=".$arsc_language."&arsc_error=waitformail&arsc_user=".$arsc_user;
-  }
-  mail($arsc_email, $arsc_lang["register_emailtemplate_subject"], str_replace("{chatowner}", $arsc_parameters["register_owner"], str_replace("{homepage}", $arsc_parameters["register_homepage"], str_replace("{password}", $arsc_password, str_replace("{username}", $arsc_user, $arsc_lang["register_emailtemplate"])))), "From: ".$arsc_parameters["register_owner_email"]);
-  mysql_query("INSERT INTO arsc_registered_users (user, password, email) VALUES ('$arsc_user', '$arsc_password', '$arsc_email')");
-  header($arsc_location);
+  header("Location: changepassword.php?arsc_language=".$arsc_language."&arsc_error=wrong_credentials&arsc_user=".$arsc_user);
   die();
  }
  else
  {
-  header("Location: register.php?arsc_email=".$arsc_email."&arsc_language=".$arsc_language."&arsc_error=register_double_user");
+  header("Location: home.php?arsc_language=".$arsc_language."&arsc_error=password_changed&arsc_user=".$arsc_user);
+  mysql_query("UPDATE arsc_registered_users SET password = '$arsc_password_new' WHERE user = '$arsc_user' AND password = '$arsc_password_old'");
   die();
  }
 }
