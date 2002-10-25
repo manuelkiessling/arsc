d9f214ce664554004c254059737ef69112132c5e
Manuel Kießling <manuel@kiessling.net>
2002-10-25 09:38:29 +0000

    added target=_blank to powered by link
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@175 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/latest/index.php
+++ trunk/latest/index.php
@@ -44,7 +44,7 @@
   <center>
    <table align="center" cellpadding="1" cellspacing="0" border="0" bgcolor="#EEEEEE">
     <tr>
-     <td><a href="http://manuel.kiessling.net/projects/software/arsc/" title="The Homepage of ARSC"><img src="pic/arsc_poweredby102x47.jpg" width="102" height="47" border="0" alt="Powered by ARSC!"></a></td>
+     <td><a href="http://manuel.kiessling.net/projects/software/arsc/" target="_blank" title="The Homepage of ARSC"><img src="pic/arsc_poweredby102x47.jpg" width="102" height="47" border="0" alt="Powered by ARSC!"></a></td>
     </tr>
    </table>
   </center>
