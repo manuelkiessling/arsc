1509e1746edd8562d07fe6f66fa77fa412835113
Manuel Kießling <manuel@kiessling.net>
2002-10-25 09:26:38 +0000

    added target=_blank to external links
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@170 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/latest/counter.php
+++ trunk/latest/counter.php
@@ -10,7 +10,7 @@
   <h2>
    Thanks!
   </h2>
-  You should now see your installation listed <a href="http://manuel.kiessling.net/projects/software/arsc/refererlist.php" title="Where is ARSC used?">here</a>.
+  You should now see your installation listed <a href="http://manuel.kiessling.net/projects/software/arsc/refererlist.php" title="Where is ARSC used?" target="_blank">here</a>.
   <br>
   <br>
   To proceed installation, go to the <a href="admin/">Admin page</a>.
