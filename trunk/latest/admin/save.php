95c54913e78f1c441496ff199c474972a0c1337c
Manuel Kießling <manuel@kiessling.net>
2002-11-01 12:21:23 +0000

    added workaround for bug #631324 added socket server restart note
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@196 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/latest/admin/save.php
+++ trunk/latest/admin/save.php
@@ -3,7 +3,7 @@
 include("../config.inc.php");
 include("../functions.inc.php");
 
-if (!$arsc_admin_password == $arsc_parameters["selfop_password"]) 
+if (!$arsc_admin_password == $arsc_parameters["selfop_password"])
 {
  ?>
   <form action="index.php" method="POST">
@@ -16,6 +16,9 @@ if (!$arsc_admin_password == $arsc_parameters["selfop_password"])
 }
 
 set_magic_quotes_runtime(1);
+
+$arsc_save_parameters["title"] = str_replace("'", "&quot;", $arsc_save_parameters["title"]);
+$arsc_save_parameters["title"] = str_replace("\2", "&quot;", $arsc_save_parameters["title"]);
 
 reset($arsc_save_parameters);
 while (list($key, $val) = each($arsc_save_parameters))
@@ -43,7 +46,7 @@ while (list($key, $val) = each($arsc_save_parameters_smilies))
  }
 }
 
-header("Location: ./?arsc_admin_password=".urlencode($arsc_admin_password)."&arsc_message=".urlencode("Parameters successfully saved."));
+header("Location: ./?arsc_admin_password=".urlencode($arsc_admin_password)."&arsc_message=".urlencode("Parameters successfully saved.<br>Please note that if you use the socket server, it needs to be restarted for most changes to take effect."));
 die();
 
 ?>
