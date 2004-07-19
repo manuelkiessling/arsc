2ee1856c0da690a7b07c5bd3bf79220f4d9e8546
Manuel Kießling <manuel@kiessling.net>
2004-07-19 21:33:53 +0000

    Fixed Bug #896643 (really a feature request) Added mysql_escape_string in queries
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@272 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/clients/shared/browser/changecolor.php
+++ trunk/arsc3/clients/shared/browser/changecolor.php
@@ -11,7 +11,8 @@ if ($arsc_my = $arsc_api->getUserValuesBySID($arsc_sid))
 {
  if ($arsc_api->userIsValid($arsc_my["user"]) AND $arsc_api->checkCommandAllowed($arsc_my["level"], "color"))
  {
-  mysql_query("UPDATE arsc_users SET color = '$arsc_color' WHERE sid = '$arsc_sid'", ARSC_PARAMETER_DB_LINK);
+  mysql_query("UPDATE arsc_users SET color = '".mysql_escape_string($arsc_color)."' WHERE sid = '".mysql_escape_string($arsc_sid)."'", ARSC_PARAMETER_DB_LINK);
+  mysql_query("UPDATE arsc_registered_users SET color = '".mysql_escape_string($arsc_color)."' WHERE user = '".mysql_escape_string($arsc_my["user"])."'", ARSC_PARAMETER_DB_LINK);
  }
  header("Location: ".getenv("HTTP_REFERER"));
  die();
