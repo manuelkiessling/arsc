9e42a0d505e5736c546669c2d7fef0a66f53e208
Manuel Kießling <manuel@kiessling.net>
2004-08-19 22:44:48 +0000

    Added message pre processing
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@287 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/clients/shared/browser/chatins.php
+++ trunk/arsc3/clients/shared/browser/chatins.php
@@ -6,6 +6,8 @@ include("../../../base/inc/functions.inc.php");
 include("../../../base/inc/inputvalidation.inc.php");
 include("../../../base/inc/api.inc.php");
 
+include("../../../base/inc/message_preprocessing.inc.php");
+
 $arsc_api = new arsc_api_Class;
 
 $arsc_api->addTraffic("incoming", strlen($arsc_message));
@@ -17,12 +19,13 @@ if ($arsc_my = $arsc_api->getUserValuesBySID(arsc_validateinput($_GET["arsc_sid"
   or what if people chat about MySQL commands etc.?
   Thus, we will use a blacklist to filter out the nastiest things, and hope that mysql_escape_string does it's job.
  */
- //$arsc_message = arsc_validateinput(htmlentities($_GET["arsc_message"], ENT_NOQUOTES), NULL, "/[^a-zA-Z0-9_ \?!\^\*\{\}\(\)\[\]=,\.\$\-&;\"'\\\]/", 0, ARSC_PARAMETER_INPUT_MAXSIZE);
  $arsc_message = arsc_validateinput(htmlentities($_GET["arsc_message"], ENT_NOQUOTES), NULL, NULL, 0, ARSC_PARAMETER_INPUT_MAXSIZE);
  if (get_magic_quotes_gpc() == 1 OR get_magic_quotes_runtime() == 1)
  {
   $arsc_message = stripslashes($arsc_message);
  }
+ // User Preprocessing
+ $arsc_message = arsc_message_preprocessing($arsc_message);
  if ($arsc_api->handleReceivedMessage($arsc_my["sid"], $arsc_message, "../../../"))
  {
   $arsc_chatversion = arsc_validateinput($_GET["arsc_chatversion"], array("browser_push", "socket", "header_js", "header", "box", "text"), NULL, NULL, NULL);
