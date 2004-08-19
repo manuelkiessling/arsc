445735bb691c4844a644a71fbf60c9dc0a65f5d2
Manuel Kießling <manuel@kiessling.net>
2004-08-19 22:43:03 +0000

    Added message post/pre processing
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@284 e1bb756e-f405-4550-b03d-8a9c705c3ac8

---
+++ trunk/arsc3/base/inc/message_preprocessing.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+function arsc_message_preprocessing($message)
+{
+ /*
+  This function is called everytime someone posts into the chat.
+  Here you can e.g. filter out words or do other tricks with the input.
+  Don't forget to return the $message!
+ */
+ 
+ // Change URLs to links:
+ $message = preg_replace("#(^|[^\"=]{1})(http://|ftp://|mailto:|news:)([^\s<>]+)([\s\n<>]|$)#sm", "\\1<a href=\"\\2\\3\" target=\"_blank\">\\2\\3</a>\\4", $message);
+ 
+ return $message;
+}
+
+?>
