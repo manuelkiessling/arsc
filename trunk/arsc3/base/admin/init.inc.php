79c97d2f9f34969eeccb69770c29648fffe636a6
Manuel Kießling <manuel@kiessling.net>
2004-08-23 21:24:40 +0000

    Added FIXME note
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@288 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/base/admin/init.inc.php
+++ trunk/arsc3/base/admin/init.inc.php
@@ -1,4 +1,7 @@
 <?php
+
+// FIXME: We register globals ourself. Yes, this is cheating. Yes, this has to be done cleanly soon.
+
 while (list($arsc_key, $arsc_val) = each($_GET))
 {
  $$arsc_key = $arsc_val;
