b4af4925432ea1dece1ca570dee562f5478d29ae
Manuel Kießling <manuel@kiessling.net>
2004-08-29 14:36:29 +0000

    Fixed typos.
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@294 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/base/inc/message_postprocessing.inc.php
+++ trunk/arsc3/base/inc/message_postprocessing.inc.php
@@ -4,10 +4,10 @@ function arsc_message_postprocessing($user, $room, $message)
 {
  /*
   This function is called everytime someone posts into the chat.
-  You can send these data e.g. into a weblog.
+  You can send this data e.g. into a weblog.
  */
  
- // Example: Send the to a webpage
+ // Example: Send the data to a webpage
  // $fp = fopen("http://www.example.com/yourscript.php?user=".$user."&room=".$room."&message=".$message, "r");
  // fclose($fp);
 }
