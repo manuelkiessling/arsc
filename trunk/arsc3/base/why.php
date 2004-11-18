cced60ef5dbe1162060b41348e7eda1797660771
Manuel Kießling <manuel@kiessling.net>
2004-11-18 14:29:18 +0000

    Fixed wrong colors.
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@308 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/base/why.php
+++ trunk/arsc3/base/why.php
@@ -22,17 +22,17 @@ $arsc_layout = $arsc_api->getBasicLayoutValues();
   </title>
  </head>
  <body
-  bgcolor="#<?php echo $arsc_layout["default_background_color"]; ?>"
-  text="#<?php echo $arsc_layout["default_font_color"]; ?>"
-  link="#<?php echo $arsc_layout["default_font_color"]; ?>"
-  vlink="#<?php echo $arsc_layout["default_font_color"]; ?>"
-  alink="#<?php echo $arsc_layout["default_font_color"]; ?>"
+  bgcolor="<?php echo $arsc_layout["default_background_color"]; ?>"
+  text="<?php echo $arsc_layout["default_font_color"]; ?>"
+  link="<?php echo $arsc_layout["default_font_color"]; ?>"
+  vlink="<?php echo $arsc_layout["default_font_color"]; ?>"
+  alink="<?php echo $arsc_layout["default_font_color"]; ?>"
  >
   <table align="center" width="80%" bgcolor="#<?php echo $arsc_layout["default_foreground_color"]; ?>">
    <tr>
     <td>
      <div align="justify">
-      <font face="<?php echo $arsc_layout["default_font_face"]; ?>" size="<?php echo $arsc_layout["default_font_size"]; ?>" color="#<?php echo $arsc_layout["default_font_color"]; ?>">
+      <font face="<?php echo $arsc_layout["default_font_face"]; ?>" size="<?php echo $arsc_layout["default_font_size"]; ?>" color="<?php echo $arsc_layout["default_font_color"]; ?>">
        <?php echo nl2br($arsc_lang["why_kicked"]); ?>
       </font>
      </div>
