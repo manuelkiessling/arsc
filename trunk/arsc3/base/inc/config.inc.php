b70f0fed7cd9e20aa3b412705c17c4e3a13d8e3f
Manuel Kießling <manuel@kiessling.net>
2004-12-02 20:38:24 +0000

    Added default values.
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@326 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/base/inc/config.inc.php
+++ trunk/arsc3/base/inc/config.inc.php
@@ -28,19 +28,19 @@
 
 // What is the name of the user who has access to the ARSC database?
 
-   define("ARSC_PARAMETER_DB_USER", "arsc3-beta1");
+   define("ARSC_PARAMETER_DB_USER", "user");
 
 
 // And what is his password?
 
-   define("ARSC_PARAMETER_DB_PASSWORD", "arsc3-beta1");
+   define("ARSC_PARAMETER_DB_PASSWORD", "password");
 
 
 // Finally, in which database will you store the ARSC tables?
 // This database MUST exist before you use the 'install.php' script,
 // and it MUST be accessible by the user you just supplied.
 
-   define("ARSC_PARAMETER_DB_DATABASE", "arsc3-beta1");
+   define("ARSC_PARAMETER_DB_DATABASE", "database");
 
 
 // END OF CONFIGURATION
