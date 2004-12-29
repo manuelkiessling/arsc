4d73a86ccb18234d7718018b6d4923ab39198f14
Manuel Kießling <manuel@kiessling.net>
2004-12-29 15:27:28 +0000

    changed to layout system fixed things to work with 3.0 codebase
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@373 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/clients/browser_socket/index.php
+++ trunk/arsc3/clients/browser_socket/index.php
@@ -3,37 +3,19 @@
 include("../../base/inc/config.inc.php");
 include("../../base/inc/init.inc.php");
 include("../../base/inc/functions.inc.php");
+include("../../base/inc/inputvalidation.inc.php");
 include("../../base/inc/api.inc.php");
 
 $arsc_api = new arsc_api_Class;
-$arsc_my = $arsc_api->getUserValuesBySID($arsc_sid);
+$arsc_my = $arsc_api->getUserValuesBySID(arsc_validateinput($_GET["arsc_sid"], NULL, "/[^a-z0-9]/", 40, 40, __FILE__, __LINE__));
 
-$arsc_result = @mysql_query("SELECT COUNT(id) AS cnt FROM arsc_users WHERE ip = '".$arsc_my["ip"]."'", ARSC_PARAMETER_DB_LINK);
-$arsc_a = @mysql_fetch_array($arsc_result);
-$arsc_server = $arsc_a["cnt"] - 1;
-if ($arsc_server > 9) $arsc_server = 0;
-
+if ($arsc_api->userIsValid($arsc_my["user"]))
+{
+ include("../../languages/".$arsc_my["language"].".inc.php");
+ define("ARSC_PARAMETER_VIRTUALSERVERS_CURRENT", arsc_getVirtualServer($arsc_my["ip"]));
+ $arsc_current["queueframeheight"] = "0";
+ if ($arsc_my["level"] >= "20") $arsc_current["queueframeheight"] = "50%";
+ echo $arsc_api->parseLayoutTemplate("browser_socket_index", FALSE);
+}
 
 ?>
-<html>
- <head>
-  <title>
-   <?php echo ARSC_PARAMETER_TITLE; ?>
-  </title>
- </head>
- <frameset cols="193,*" border="0" framespacing="no" frameborder="0" marginwidth="2" marginheight="1">
-  <frame src="http://srv<?php echo $arsc_server; ?>.<?php echo getenv("HTTP_HOST"); ?><?php echo dirname($PHP_SELF); ?>/../shared/browser/roomlist.php?arsc_sid=<?php echo $arsc_sid; ?>" name="roomlist" scrolling="auto" noresize marginwidth="0" marginheight="0">
-  <frameset cols="*,120" border="0" framespacing="no" frameborder="0" marginwidth="2" marginheight="1">
-   <frameset rows="0,80,*,40" border="1" framespacing="no" frameborder="0" marginwidth="2" marginheight="1">
-    <frame src="../shared/browser/empty.php" NAME="empty" scrolling="no" noresize marginwidth="0" marginheight="0">
-    <frame src="http://www.christinafanclub.at/chat/kopf.php" name="external" scrolling="no" noresize marginwidth="0" marginheight="0">
-    <frame src="http://srv<?php echo $arsc_server; ?>.<?php echo ARSC_PARAMETER_SOCKETSERVER_ADRESS.":".ARSC_PARAMETER_SOCKETSERVER_PORT; ?>/?arsc_sid=<?php echo $arsc_sid; ?>" NAME="msg" scrolling="auto" noresize marginwidth="2" marginheight="0">
-    <frame src="chatinput.php?arsc_sid=<?php echo $arsc_sid; ?>" name="input" scrolling="no" noresize marginwidth="2" marginheight="1">
-   </frameset>
-   <frame src="../shared/browser/userlist.php?arsc_sid=<?php echo $arsc_sid; ?>" name="users" scrolling="auto" noresize marginwidth="2" marginheight="2">
-  </frameset>
- </frameset>
- <noframes>
-  Sorry, this version of ARSC needs a browser that understands framesets. But we have a lynx-friendly version too.
- </noframes>
-</html>
