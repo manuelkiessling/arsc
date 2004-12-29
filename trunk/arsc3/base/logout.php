fc8f9f6da22a2ec875ce47cee8cbb4c2a8639552
Manuel Kießling <manuel@kiessling.net>
2004-12-29 14:59:37 +0000

    replaced hardcoded removal of user with API function
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@358 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/base/logout.php
+++ trunk/arsc3/base/logout.php
@@ -13,15 +13,10 @@ if ($arsc_my = $arsc_api->getUserValuesBySID(arsc_validateinput($_GET["arsc_sid"
 {
  if ($arsc_post_logout == "true")
  {
-  $arsc_user = $arsc_my["user"];
-  $arsc_room = $arsc_my["room"];
-  $arsc_nice_room = $arsc_api->getReadableRoomname($arsc_room);
-  $arsc_timeid = arsc_microtime();
-  $arsc_sendtime = date("H:i:s");
-  mysql_query("INSERT INTO arsc_room_$arsc_room (message, user, sendtime, timeid) VALUES ('arsc_user_quit~~$arsc_user~~$arsc_nice_room', 'System', '$arsc_sendtime', '$arsc_timeid')", ARSC_PARAMETER_DB_LINK);
+  $arsc_api->removeUserFromRoom($arsc_my);
  }
  mysql_query("DELETE FROM arsc_users WHERE sid = '".$arsc_my["sid"]."'", ARSC_PARAMETER_DB_LINK);
- header ("Location: home.php?arsc_language=".$arsc_my["language"]);
+ header("Location: home.php?arsc_language=".$arsc_my["language"]);
  die();
 }
 else
