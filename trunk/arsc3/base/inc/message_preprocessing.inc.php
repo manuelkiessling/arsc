6da23583459a69a062157cc111701665480299bd
Manuel Kießling <manuel@kiessling.net>
2004-12-29 15:20:06 +0000

    use constant in roomtype check
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@368 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/base/inc/message_preprocessing.inc.php
+++ trunk/arsc3/base/inc/message_preprocessing.inc.php
@@ -3,19 +3,18 @@
 function arsc_message_preprocessing($message, $roomname = "", $roomtype = 1)
 {
  /*
-  This function is called every time someone posts into the chat.
+  This function is called every time someone posts into the chat, BEFORE it is
+  written to the database.
   Here you can e.g. filter out words or do other tricks with the input.
-  Don't forget to return the $message!
+  Don't forget to return the $message, or the whole chat will not work!
  */
  
  // Change URLs to HTML links, but not in moderated rooms:
- if($roomtype <> 2)
+ if($roomtype <> ARSC_ROOMTYPE_MODERATED)
  {
-  $message = preg_replace(
-                          "#(^|[^\"=]{1})(http://|https://|ftp://|mailto:|news:)([^\s<>]+)([\s\n<>]|$)#sm",
+  $message = preg_replace("#(^|[^\"=]{1})(http://|https://|ftp://|mailto:|news:)([^\s<>]+)([\s\n<>]|$)#sm",
                           "\\1<a href=\"\\2\\3\" target=\"_blank\">\\2\\3</a>\\4",
-                          $message
-                         );
+                          $message);
  }
  
  // Do something else
