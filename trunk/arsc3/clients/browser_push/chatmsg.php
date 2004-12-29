28eb2af3a2032157e8a03eb971c36709818b569f
Manuel Kießling <manuel@kiessling.net>
2004-12-29 15:24:03 +0000

    fixed shutdown functionality
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@371 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/clients/browser_push/chatmsg.php
+++ trunk/arsc3/clients/browser_push/chatmsg.php
@@ -1,6 +1,6 @@
 <?php
 
-set_time_limit(0);
+if (ini_get("safe_mode") == 0) set_time_limit(0);
 set_magic_quotes_runtime(0);
 ob_end_flush();
 
@@ -16,18 +16,8 @@ register_shutdown_function("arsc_shutdown");
 
 function arsc_shutdown()
 {
- GLOBAL $arsc_sid,
-        $arsc_my;
- if ($arsc_my["user"] <> "")
- {
-  $arsc_user = $arsc_my["user"];
-  $arsc_room = $arsc_my["room"];
-  $arsc_nice_room = arsc_nice_room($arsc_room);
-  $arsc_timeid = arsc_microtime();
-  $arsc_sendtime = date("H:i:s");
-  mysql_query("DELETE FROM arsc_users WHERE sid = '".mysql_escape_string($arsc_sid)."'", ARSC_PARAMETER_DB_LINK);
-  mysql_query("INSERT INTO arsc_room_$arsc_room (message, user, sendtime, timeid) VALUES ('".mysql_escape_string("arsc_user_quit~~".$arsc_user."~~".$arsc_nice_room)."', 'System', '".mysql_escape_string($arsc_sendtime)."', '".mysql_escape_string($arsc_timeid)."')", ARSC_PARAMETER_DB_LINK);
- }
+ GLOBAL $arsc_api, $arsc_my;
+ $arsc_api->removeUserFromRoom($arsc_my);
 }
 
 function arsc_getmessages($arsc_sid)
@@ -41,14 +31,13 @@ function arsc_getmessages($arsc_sid)
  {
   if (!$arsc_api->userIsValid($arsc_my["user"]))
   {
-   $arsc_api->deleteUser($arsc_my["user"]);
-   return arsc_filter_posting("System", date("H:i:s"), $arsc_lang["youwerekicked"], $arsc_my["room"], 0, 1, $arsc_template_xml);
+   $arsc_api->removeUserFromRoom($arsc_my);
   }
   else
   {
    $arsc_posting = "\n";
    $arsc_lastmessageping = $arsc_api->getUserValueByName("lastmessageping", $arsc_my["user"]);
-   if ($arsc_lastmessageping == "0")
+   if ($arsc_lastmessageping == 0)
    {
     $arsc_api->setUserValueByName("lastmessageping", arsc_microtime(), $arsc_my["user"]);
     $arsc_api->setUserValueByName("lastping", time(), $arsc_my["user"]);
@@ -63,16 +52,13 @@ function arsc_getmessages($arsc_sid)
     if ($arsc_messages[1] <> "") $arsc_api->setUserValueByName("lastmessageping", $arsc_messages[1], $arsc_my["user"]);
     $arsc_api->setUserValueByName("lastping", time(), $arsc_my["user"]);
    }
-   if ($arsc_my["version"] <> "external")
+   $arsc_posting = str_replace("#ret#", "\n", $arsc_posting);
+   if ($arsc_posting <> "\n")
    {
-    $arsc_posting = str_replace("#ret#", "\n", $arsc_posting);
-    if ($arsc_posting <> "\n")
+    if (ARSC_PARAMETER_SMILIES == "yes" AND $arsc_api->checkCommandAllowed($arsc_my["level"], "smilies"))
     {
-     if (ARSC_PARAMETER_SMILIES == "yes" AND $arsc_api->checkCommandAllowed($arsc_my["level"], "smilies"))
-     {
-      reset($arsc_smilies);
-      $arsc_posting = arsc_smilies_replace($arsc_posting, $arsc_smilies, ARSC_PARAMETER_SMILIES_PATH);
-     }
+     reset($arsc_smilies);
+     $arsc_posting = arsc_smilies_replace($arsc_posting, $arsc_smilies, ARSC_PARAMETER_SMILIES_PATH);
     }
    }
    return str_replace("#arsc_dummy_space#", "", $arsc_posting);
@@ -92,14 +78,14 @@ if ($arsc_my = $arsc_api->getUserValuesBySID(arsc_validateinput($_GET["arsc_sid"
   }
   echo ARSC_PARAMETER_HTMLHEAD_JS;
   $arsc_template_varname = "arsc_template_".$arsc_my["template"];
-  echo arsc_filter_posting("System", date("H:i:s"), "/msg ".$arsc_my["user"]." ".str_replace("{title}", ARSC_PARAMETER_TITLE, $arsc_lang["welcome"]), $arsc_my["room"], 0, 0, $$arsc_template_varname);
-  echo arsc_filter_posting("System", date("H:i:s"), "/msg ".$arsc_my["user"]." ".ARSC_PARAMETER_WELCOME_MESSAGE, $arsc_my["room"], 0, 0, $$arsc_template_varname);
+  echo arsc_filter_posting("System", date("H:i:s"), "/msg ".$arsc_my["user"]." ".str_replace("{title}", ARSC_PARAMETER_TITLE, $arsc_lang["welcome"]), $arsc_my["room"], 0, 0, 0, $$arsc_template_varname);
+  if(ARSC_PARAMETER_WELCOME_MESSAGE <> "") echo arsc_filter_posting("System", date("H:i:s"), "/msg ".$arsc_my["user"]." ".ARSC_PARAMETER_WELCOME_MESSAGE, $arsc_my["room"], 0, 0, 0, $$arsc_template_varname);
   flush();
   while (!connection_aborted())
   {
    echo arsc_getmessages($arsc_my["sid"]).$arsc_compatibility_hack;
-   usleep(ARSC_PARAMETER_SOCKETSERVER_REFRESH);
    flush();
+   usleep(ARSC_PARAMETER_SOCKETSERVER_REFRESH);
   }
  }
 }
