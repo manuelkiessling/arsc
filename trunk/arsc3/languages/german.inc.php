531e6ff8cac30e2862ff904b4146c2742cb10393
Manuel Kießling <manuel@kiessling.net>
2005-01-08 13:08:09 +0000

    changed from 3.0-beta3 to 3.0-rc1
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@404 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/languages/german.inc.php
+++ trunk/arsc3/languages/german.inc.php
@@ -5,7 +5,7 @@
  a copy to <manuel@kiessling.net>, I will add it to ARSC then.
  Thanks.
 
- This file is for versions 3.0-beta1, 3.0-beta2, 3.0-beta3
+ This file is for versions 3.0-beta1, 3.0-beta2, 3.0-rc1
   
  Translation by Manuel Kiessling <manuel@kiessling.net>.
 */
