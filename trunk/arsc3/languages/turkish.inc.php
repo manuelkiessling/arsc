4d8e077becc359a354952194b12e322ad1639e17
Manuel Kießling <manuel@kiessling.net>
2005-01-17 15:19:19 +0000

    Added version number for 3.0
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@418 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/languages/turkish.inc.php
+++ trunk/arsc3/languages/turkish.inc.php
@@ -5,7 +5,7 @@
  a copy to <manuel@kiessling.net>, I will add it to ARSC then.
  Thanks.
 
- This file is for versions 3.0-beta1, 3.0-beta2, 3.0-rc1
+ This file is for versions 3.0-beta1, 3.0-beta2, 3.0-rc1, 3.0
   
  Translation by Ridvan Agar <ridvan.agar@web.de>.
 */
