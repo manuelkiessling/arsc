3147e6695b9596cdb1659aa5980d01f15714184b
Manuel Kießling <manuel@kiessling.net>
2005-01-18 22:39:24 +0000

    Added password support
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@425 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/clients/external_scripts/index.php
+++ trunk/arsc3/clients/external_scripts/index.php
@@ -13,6 +13,12 @@ $arsc_api = new arsc_api_Class;
 $arsc_room = arsc_validateinput($_GET["arsc_room"], NULL, "/[^a-z0-9_\-]/", 0, ARSC_PARAMETER_INPUT_MAXSIZE, __FILE__, __LINE__);
 $arsc_message = arsc_validateinput($_GET["arsc_message"], NULL, NULL, 0, ARSC_PARAMETER_INPUT_MAXSIZE, __FILE__, __LINE__);
 $arsc_user = arsc_validateinput($_GET["arsc_user"], NULL, "/[^a-zA-Z0-9_\-]/", 0, ARSC_PARAMETER_INPUT_MAXSIZE, __FILE__, __LINE__);
+$arsc_password = arsc_validateinput($_GET["arsc_password"], NULL, "/[^a-zA-Z0-9_\-]/", 0, ARSC_PARAMETER_INPUT_MAXSIZE, __FILE__, __LINE__);
+$arsc_uservalues = $arsc_api->getRegisteredUserValuesByName($arsc_user);
+if($arsc_uservalues == FALSE OR sha1($arsc_password) <> $arsc_uservalues["password"])
+{
+ arsc_error_log(ARSC_ERRORLEVEL_FATAL, "External user ".$arsc_user." could not be authenticated", __FILE__, __LINE__);
+}
 $arsc_message = arsc_message_preprocessing($arsc_message, $arsc_room, $arsc_api->getRoomType($arsc_room));
 if (get_magic_quotes_gpc() == 1 OR get_magic_quotes_runtime() == 1)
 {
