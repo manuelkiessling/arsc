ce55538637683906097df8701c85194fada9c2a5
Manuel Kießling <manuel@kiessling.net>
2005-01-18 22:38:42 +0000

    initial commit
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@424 e1bb756e-f405-4550-b03d-8a9c705c3ac8

---
+++ trunk/arsc3/clients/external_scripts/input.php
@@ -0,0 +1,11 @@
+<form action="index.php">
+ user: <input type="text" name="arsc_user" value="TheBot">
+ <br>
+ room: <input type="text" name="arsc_room" value="free_for_all">
+ <br>
+ text: <input type="text" name="arsc_message">
+ <br>
+ password: <input type="text" name="arsc_password" value="dKi876gC">
+ <br>
+ <input type="submit">
+</form>
