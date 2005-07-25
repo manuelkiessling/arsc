f3dc3162cb172f899af4e963c6b2db0f630ab5b6
Manuel Kießling <manuel@kiessling.net>
2005-07-25 20:06:07 +0000

    made browser_refresh working
    git-svn-id: svn://svn.code.sf.net/p/arsc/code@488 e1bb756e-f405-4550-b03d-8a9c705c3ac8

--- trunk/arsc3/clients/browser_refresh/chatmsg.php
+++ trunk/arsc3/clients/browser_refresh/chatmsg.php
@@ -9,75 +9,54 @@ include("../../base/inc/filter.inc.php");
 
 $arsc_api = new arsc_api_Class;
 
-if ($arsc_my = $arsc_api->getUserValuesBySID(arsc_validateinput($_GET["arsc_sid"], NULL, "/[^a-z0-9]/", 40, 40, __FILE__, __LINE__)))
+if($arsc_my = $arsc_api->getUserValuesBySID(arsc_validateinput($_GET["arsc_sid"], NULL, "/[^a-z0-9]/", 40, 40, __FILE__, __LINE__)))
 {
  include("../../languages/".$arsc_my["language"].".inc.php");
  
- if (!$arsc_api->userIsValid($arsc_my["user"]))
+ if($arsc_my["lastmessageping"] == 0)
  {
-  $arsc_api->removeUserFromRoom($arsc_my);
-  header("Expires: Sun, 28 Dec 1997 09:32:45 GMT");
-  header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
-  header("Cache-Control: no-cache, must-revalidate");
-  header("Pragma: no-cache");
-  header("Content-Type: text/html");
-  ?>
-  <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-  <html>
-   <body>
-    <?php echo arsc_filter_posting("System", date("H:i:s"), $arsc_lang["youwerekicked"], $arsc_my["room"], 0, 0, 0, "arsc_template_html"); ?>
-   </body>
-  </html>
-  <?php
+  $arsc_my["lastmessageping"] = $arsc_api->setLastMessagePing($arsc_my);
  }
- else
+ 
+ if($_GET["dummy"] == "")
  {
-  if($_GET["dummy"] == "")
-  {
-   $arsc_api->setLastMessagePing($arsc_my);
-   $arsc_my = $arsc_api->getUserValuesBySID($arsc_my["sid"]);
-   $arsc_api->postMessage($arsc_my["room"], "arsc_user_enter~~".$arsc_my["user"]."~~".$arsc_api->getReadableRoomname($arsc_my["room"]), "System", date("H:i:s"), arsc_microtime(), 0);
-  }
-  header("Expires: Sun, 28 Dec 1997 09:32:45 GMT");
-  header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
-  header("Cache-Control: no-cache, must-revalidate");
-  header("Pragma: no-cache");
-  header("Refresh: 4; URL=chatmsg.php?arsc_sid=".$arsc_my["sid"]."&dummy=".time()."#end");
-  header("Content-Type: text/html");
-
-  // We get them messages twice, because of roomchanges
-  $arsc_messages = $arsc_api->getMessages($arsc_my["lastmessageping"], $arsc_my["room"], $arsc_my["template"], "ASC");
-  ?>
-  <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-  <html>
-   <head>
-    <title>
-     <?php echo ARSC_PARAMETER_TITLE; ?>
-    </title>
-   </head>
-   <body>
-    <?php
-    set_magic_quotes_runtime(0);
-    echo $arsc_messages[0];
-    $arsc_template_varname = "arsc_template_".$arsc_my["template"];
-    if($_GET["dummy"] == "")
-    {
-     if(ARSC_PARAMETER_WELCOME_MESSAGE <> "") echo arsc_filter_posting("System", date("H:i:s"), "/msg ".$arsc_my["user"]." ".ARSC_PARAMETER_WELCOME_MESSAGE, $arsc_my["room"], 0, 0, 0, $$arsc_template_varname);
-     echo arsc_filter_posting("System", date("H:i:s"), "/msg ".$arsc_my["user"]." ".str_replace("{title}", ARSC_PARAMETER_TITLE, $arsc_lang["welcome"]), $arsc_my["room"], 0, 0, 0, $$arsc_template_varname);
-    }
-    $arsc_api->setUserValueByName("lastping", time(), $arsc_my["user"]);
-    $arsc_api->setUserValueByName("ip", getenv("REMOTE_ADDR"), $arsc_my["user"]);
-    ?>
-    <a name="end"></a>
-   </body>
-  </html>
-  <?php
+  $arsc_api->setLastMessagePing($arsc_my);
+  $arsc_my = $arsc_api->getUserValuesBySID($arsc_my["sid"]);
+  $arsc_api->postMessage($arsc_my["room"], "arsc_user_enter~~".$arsc_my["user"]."~~".$arsc_api->getReadableRoomname($arsc_my["room"]), "System", date("H:i:s"), arsc_microtime(), 0);
  }
-}
-else
-{
- header("Location: ../../base/why.php?arsc_language=".$arsc_language);
- die();
-}
+ header("Expires: Sun, 28 Dec 1997 09:32:45 GMT");
+ header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+ header("Cache-Control: no-cache, must-revalidate");
+ header("Pragma: no-cache");
+ header("Refresh: 4; URL=chatmsg.php?arsc_sid=".$arsc_my["sid"]."&dummy=".time()."#end");
+ header("Content-Type: text/html");
 
+ $arsc_messages = $arsc_api->getMessages($arsc_my["lastmessageping"], $arsc_my["room"], $arsc_my["template"], "ASC");
+ //$arsc_my["lastmessageping"] = $arsc_api->setLastMessagePing($arsc_my);
+ ?>
+ <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+ <html>
+  <head>
+   <title>
+    <?php echo ARSC_PARAMETER_TITLE; ?>
+   </title>
+  </head>
+  <body>
+   <?php
+   set_magic_quotes_runtime(0);
+   echo $arsc_messages[0];
+   $arsc_template_varname = "arsc_template_".$arsc_my["template"];
+   if($_GET["dummy"] == "")
+   {
+    if(ARSC_PARAMETER_WELCOME_MESSAGE <> "") echo arsc_filter_posting("System", date("H:i:s"), "/msg ".$arsc_my["user"]." ".ARSC_PARAMETER_WELCOME_MESSAGE, $arsc_my["room"], 0, 0, 0, $$arsc_template_varname);
+    echo arsc_filter_posting("System", date("H:i:s"), "/msg ".$arsc_my["user"]." ".str_replace("{title}", ARSC_PARAMETER_TITLE, $arsc_lang["welcome"]), $arsc_my["room"], 0, 0, 0, $$arsc_template_varname);
+   }
+   $arsc_api->setUserValueByName("lastping", time(), $arsc_my["user"]);
+   $arsc_api->setUserValueByName("ip", getenv("REMOTE_ADDR"), $arsc_my["user"]);
+   ?>
+   <a name="end"></a>
+  </body>
+ </html>
+ <?php
+}
 ?>
